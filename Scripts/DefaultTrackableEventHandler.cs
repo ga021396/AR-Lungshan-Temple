c31d84198064d5e41b30d4fb84ea342ed03ab7f4
Unknown <ga021396@gmail.com>
2018-05-06 13:33:10 +0800

    整合版01

---
+++ Scripts/DefaultTrackableEventHandler.cs
@@ -0,0 +1,144 @@
+/*==============================================================================
+Copyright (c) 2017 PTC Inc. All Rights Reserved.
+
+Copyright (c) 2010-2014 Qualcomm Connected Experiences, Inc.
+All Rights Reserved.
+Confidential and Proprietary - Protected under copyright and other laws.
+==============================================================================*/
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Vuforia;
+
+/// <summary>
+///     A custom handler that implements the ITrackableEventHandler interface.
+/// </summary>
+public class DefaultTrackableEventHandler : MonoBehaviour, ITrackableEventHandler
+{
+    #region PRIVATE_MEMBER_VARIABLES
+
+    protected TrackableBehaviour mTrackableBehaviour;
+
+    #endregion // PRIVATE_MEMBER_VARIABLES
+
+    #region UNTIY_MONOBEHAVIOUR_METHODS
+
+    protected virtual void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+    }
+
+    #endregion // UNTIY_MONOBEHAVIOUR_METHODS
+
+    #region PUBLIC_METHODS
+
+    /// <summary>
+    ///     Implementation of the ITrackableEventHandler function called when the
+    ///     tracking state changes.
+    /// </summary>
+    public void OnTrackableStateChanged(
+        TrackableBehaviour.Status previousStatus,
+        TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " found");
+            OnTrackingFound();
+        }
+        else if (previousStatus == TrackableBehaviour.Status.TRACKED &&
+                 newStatus == TrackableBehaviour.Status.NOT_FOUND)
+        {
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " lost");
+            OnTrackingLost();
+        }
+        else
+        {
+            // For combo of previousStatus=UNKNOWN + newStatus=UNKNOWN|NOT_FOUND
+            // Vuforia is starting, but tracking has not been lost or found yet
+            // Call OnTrackingLost() to hide the augmentations
+            OnTrackingLost();
+        }
+    }
+
+    #endregion // PUBLIC_METHODS
+
+    #region PRIVATE_METHODS
+
+    protected virtual void OnTrackingFound()
+    {
+        var rendererComponents = GetComponentsInChildren<Renderer>(true);
+        var colliderComponents = GetComponentsInChildren<Collider>(true);
+        var canvasComponents = GetComponentsInChildren<Canvas>(true);
+
+        // Enable rendering:
+        foreach (var component in rendererComponents)
+            component.enabled = true;
+
+        // Enable colliders:
+        foreach (var component in colliderComponents)
+            component.enabled = true;
+
+        // Enable canvas':
+        foreach (var component in canvasComponents)
+            component.enabled = true;
+
+        targetName();
+        //-----------------------------------------------------
+   
+        //-----------------------------------------------------
+    }
+
+
+    protected virtual void OnTrackingLost()
+    {
+        var rendererComponents = GetComponentsInChildren<Renderer>(true);
+        var colliderComponents = GetComponentsInChildren<Collider>(true);
+        var canvasComponents = GetComponentsInChildren<Canvas>(true);
+
+        // Disable rendering:
+        foreach (var component in rendererComponents)
+            component.enabled = false;
+
+        // Disable colliders:
+        foreach (var component in colliderComponents)
+            component.enabled = false;
+
+        // Disable canvas':
+        foreach (var component in canvasComponents)
+            component.enabled = false;
+    }
+    //------------------------------------------------------------
+    public void targetName() {
+        //SceneManager.LoadScene("Introduce");
+
+        GameObject panel;
+        panel = GameObject.Find("Panel");
+        Animator anim = panel.GetComponent<Animator>();
+        anim.Play("move");
+        //ζ“Ύ‰ξΠα`–Κ•ΐ·s“®α`
+
+        Text text;
+        text = GameObject.Find("History").GetComponent<Text>();
+        //ζ“Ύ‰ξΠ•¶
+
+        UnityEngine.UI.Image img;
+        Sprite Myimg;
+        img = GameObject.Find("Photo").GetComponent<UnityEngine.UI.Image>();
+        //ζ“Ύ¤•Π 
+
+        if (mTrackableBehaviour.TrackableName == "SideBuilding")
+        {   
+            text.text ="";
+            Myimg = Resources.Load<Sprite>("IMG2");
+            img.sprite = Myimg;
+            //Λ–Ϊ•WX·¤•Π‹y•¶
+        }
+
+    }
+    #endregion // PRIVATE_METHODS
+}
