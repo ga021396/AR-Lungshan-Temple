15099b52c467145049bcd2186c89eaae93f9ce0c
Unknown <ga021396@gmail.com>
2018-05-16 13:36:15 +0800

    初評程式碼

--- Scripts/DefaultTrackableEventHandler.cs
+++ Scripts/DefaultTrackableEventHandler.cs
@@ -16,6 +16,7 @@ using Vuforia;
 /// </summary>
 public class DefaultTrackableEventHandler : MonoBehaviour, ITrackableEventHandler
 {
+    private GameObject videoplay;
     #region PRIVATE_MEMBER_VARIABLES
 
     protected TrackableBehaviour mTrackableBehaviour;
@@ -23,13 +24,36 @@ public class DefaultTrackableEventHandler : MonoBehaviour, ITrackableEventHandle
     #endregion // PRIVATE_MEMBER_VARIABLES
 
     #region UNTIY_MONOBEHAVIOUR_METHODS
+    
+    //function to stop all sounds
+    void PlayAllAudio()
+    {
+        allAudioSources = FindObjectsOfType(typeof(AudioSource)) as AudioSource[];
+        foreach (AudioSource audioS in allAudioSources)
+        {
+            if (audioS.isPlaying)
+                audioS.Stop();
+            else
+                audioS.Play();
+        }
+    }
+    public void StopAllAudio()
+    {
+        allAudioSources = FindObjectsOfType(typeof(AudioSource)) as AudioSource[];
+        foreach (AudioSource audioS in allAudioSources)
+        {
+            if (audioS.isPlaying)
+                audioS.Stop();
+        }
+    }
 
     protected virtual void Start()
     {
         mTrackableBehaviour = GetComponent<TrackableBehaviour>();
         if (mTrackableBehaviour)
             mTrackableBehaviour.RegisterTrackableEventHandler(this);
-    }
+     
+}
 
     #endregion // UNTIY_MONOBEHAVIOUR_METHODS
 
@@ -68,6 +92,10 @@ public class DefaultTrackableEventHandler : MonoBehaviour, ITrackableEventHandle
     #endregion // PUBLIC_METHODS
 
     #region PRIVATE_METHODS
+    void Awake()
+    {
+        videoplay = GameObject.Find("videoplay");
+    }
 
     protected virtual void OnTrackingFound()
     {
@@ -87,7 +115,9 @@ public class DefaultTrackableEventHandler : MonoBehaviour, ITrackableEventHandle
         foreach (var component in canvasComponents)
             component.enabled = true;
 
+        
         targetName();
+      
         //-----------------------------------------------------
    
         //-----------------------------------------------------
@@ -111,15 +141,22 @@ public class DefaultTrackableEventHandler : MonoBehaviour, ITrackableEventHandle
         // Disable canvas':
         foreach (var component in canvasComponents)
             component.enabled = false;
+
+        videoplay.SetActive(false);
     }
     //------------------------------------------------------------
+    AudioClip myClip = new AudioClip();
+    AudioSource mySource;
+    private AudioSource[] allAudioSources;
+
     public void targetName() {
         //SceneManager.LoadScene("Introduce");
+             
 
         GameObject panel;
         panel = GameObject.Find("Panel");
         Animator anim = panel.GetComponent<Animator>();
-        anim.Play("move");
+       
         //éÊìæâÓè–·`ñ ï¿é∑çsìÆ·`
 
         Text text;
@@ -131,14 +168,50 @@ public class DefaultTrackableEventHandler : MonoBehaviour, ITrackableEventHandle
         img = GameObject.Find("Photo").GetComponent<UnityEngine.UI.Image>();
         //éÊìæö§ï– 
 
+         mySource = GameObject.Find("Audio Source").GetComponent<AudioSource>();
+        //éÊìæåÍâπ
+
         if (mTrackableBehaviour.TrackableName == "SideBuilding")
-        {   
-            text.text ="";
-            Myimg = Resources.Load<Sprite>("IMG2");
+        {
+            text.text = "This is where we show you all the amazing stuff that just arrived at our warehouse. Stay up-to-date with all the latest gear right here, or, if you prefer, you can get this same info via our What's New RSS feed or by following @thinkgeekspam.";
+            Myimg = Resources.Load<Sprite>("image/IMG2");
             img.sprite = Myimg;
+            anim.Play("move");
             //àÀñ⁄ïWçXä∑ö§ï–ãyï∂éö
+
+            myClip = (AudioClip)Resources.Load("audios/audio00");
+            mySource.clip = myClip;
+            mySource.Play();
+            StopAllAudio();
         }
 
-    }
+        if (mTrackableBehaviour.TrackableName == "totem")
+        {
+            text.text = "High Energy Physics - Theory. New submissions. Submissions received from Fri 4 May 18 to Mon 7 May 18, announced Tue, 8 May 18. New submissions; Cross-lists; Replacements.";
+            Myimg = Resources.Load<Sprite>("image/images");
+            img.sprite = Myimg;
+            anim.Play("move");
+            //àÀñ⁄ïWçXä∑ö§ï–ãyï∂éö
+
+            myClip = (AudioClip)Resources.Load("audios/audio02");
+            mySource.clip = myClip;
+            mySource.Play();
+            StopAllAudio();
+        }
+        if (mTrackableBehaviour.TrackableName == "stele") {
+            videoplay.SetActive(true);
+        }
+
+        }   
+    public void onClick() {
+        PlayAllAudio();
+        /*AudioClip myClip = new AudioClip();
+        myClip = (AudioClip)Resources.Load("audio01");
+        AudioSource mySource = GameObject.Find("Audio Source").GetComponent<AudioSource>();
+        mySource.clip = myClip;
+        mySource.Play();
+        mySource.enabled = false;
+    */}
+   
     #endregion // PRIVATE_METHODS
 }
---
+++ Scripts/Videocontroll.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class Videocontroll : MonoBehaviour {
+    private VideoPlayer videoPlayer;
+    private GameObject videoplay;
+    private AudioSource audioS;
+   
+    
+    
+    // Use this for initialization
+    void Awake() {
+        videoplay = GameObject.Find("videoplay");
+
+        videoPlayer = GetComponent<VideoPlayer>();
+        videoPlayer.playOnAwake = false;
+        audioS = GetComponent<AudioSource>();
+    }
+    void Start () {
+        
+
+    }
+	// Update is called once per frame
+	void Update () {
+        
+         if (videoplay.activeSelf)
+         {
+            if (videoPlayer.isPlaying)
+                return;
+            else
+            {
+                videoPlayer.Play();
+                audioS.Play();
+            }
+         }
+         else
+         {
+            audioS.Stop();
+             videoPlayer.Stop();
+        }
+         
+    }
+}
